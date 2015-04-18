cf0cf35b04f6d502bbd9d02fb08815031aec8957
jvaningen <jane_vaningen@hotmail.com>
2015-04-18 10:55:51 -0400

    test

---
+++ OnlineDatingProfile/js/project.js
@@ -0,0 +1,10 @@
+
+var message = "";
+
+function addMessage() {
+document.getElementById("message").value = "me and Rory";
+document.getElementById("images/LorelaiRory.jpg").innerHTML= addMessage;
+  
+}
+
+document.getElementById("images/LorelaiRory.jpg").onmouseover = addMessage;
